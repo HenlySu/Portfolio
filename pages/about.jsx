e563cc38888224018150131975e0ca4fef1a79ac
HenlySuu <hsu31@myseneca.ca>
2023-06-13 00:25:53 -0400

    update

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,7 +1,6 @@
 import { Container, Row } from "react-bootstrap"
 import About from "@/src/components/About/About";
 import Skill from "@/src/components/About/Skills";
-import Education from "@/src/components/About/Education";
 import Github from "@/src/components/About/Github";
 
 export default function AboutPage(){
@@ -11,7 +10,6 @@ export default function AboutPage(){
             <Row>
                <About />
                <Skill />
-               <Education />
                <Github />
             </Row>
          </Container>
--- src/components/About/Github.jsx
+++ src/components/About/Github.jsx
@@ -1,11 +1,12 @@
 import { Row } from "react-bootstrap"
 import GitHubCalendar from "react-github-calendar"
+import style from "./about.module.css"
 
 export default function Github(){
    return (
       <>
          <Row>
-            <h5>The days I&#39;ve coded</h5>
+            <h3 className={`mt-4 ${style.arsenal}`}>The days I&#39;ve coded</h3>
             <GitHubCalendar username="HenlySu" />
          </Row>
       </>
